f07650b7f0ee46f6dcb141aa0d264bf6d1555355
baophandev <phangiabao.fw@gmail.com>
2025-01-27 21:37:30 +0700

    adjust review mapper

--- src/main/java/com/example/PhoneShop/mapper/ReviewMapper.java
+++ src/main/java/com/example/PhoneShop/mapper/ReviewMapper.java
@@ -5,8 +5,12 @@ import com.example.PhoneShop.dto.response.ReviewResponse;
 import com.example.PhoneShop.entities.Product;
 import com.example.PhoneShop.entities.Review;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
+
+    @Mapping(target = "displayName", source = "user.displayName")
+    @Mapping(target = "prdId", source = "product.id")
     ReviewResponse toReviewResponse(Review review);
 }
